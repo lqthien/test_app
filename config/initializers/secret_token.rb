0daab0331e54cd8fe422e0f36c4a2db0802cf5f4
Thien.Lam <thien.lam@eastagile.com>
2012-11-10 09:58:52 +0700

    Init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = 'bb3888a6017df8ac4dbcb994b8e4351726cec23a1aa1c0197eed23ba1209efa6fe8dfc92aa24d0a8e679877b975f766aa43da04ca3102d6083d0616a1d563fbc'
