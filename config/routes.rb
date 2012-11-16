3260ef08d36bbc5372e658ba0c8b7360f7e31169
Thien.Lam <thien.lam@eastagile.com>
2012-11-17 00:16:03 +0700

    change route and title

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,7 @@ TestApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => "dashboard#welcome"
 
   # See how all your routes lay out with "rake routes"
 
