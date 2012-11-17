40f9a1c5f0334033d6952650b0641d2074dc6f9e
Thien.Lam <thien.lam@eastagile.com>
2012-11-17 07:30:09 +0700

    Ajax interface

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,4 +2,10 @@ class DashboardController < ApplicationController
   def welcome
 
   end
+
+  def switch
+    respond_to do |format|
+      format.js
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 TestApp::Application.routes.draw do
   match ':controller(/:action(/:id))'
+  controller :dashboard do
+    post '/dashboard/switch' => :switch
+  end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
